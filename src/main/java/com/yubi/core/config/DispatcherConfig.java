27d5aaa68299ae7f40edd2eedf38071345296c4a
Alex <alex@barnes-home.com>
2012-12-08 16:44:15 -0500

    Configure the DispatcherServlet to be the default
    This is to avoid 404s when old URLs can't be found even though they've
    been indexed by Google.

--- src/main/java/com/yubi/core/config/DispatcherConfig.java
+++ src/main/java/com/yubi/core/config/DispatcherConfig.java
@@ -3,6 +3,7 @@ package com.yubi.core.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -32,5 +33,11 @@ public class DispatcherConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public StandardServletMultipartResolver multipartResolver() {
 		return new StandardServletMultipartResolver();
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer configurer) {
+		configurer.enable("appServlet");
 	}
 }
